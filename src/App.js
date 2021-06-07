0f76170dada0b3f499233aa7a9d3b40779ef1e63
miumiu5228 <miumiu5228@gmail.com>
2021-06-06 17:34:33 -0700

    clickEvent

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  // let name = 'Ling';
+  const [name, setName] = useState('Ling');
+
+  const handleClick = () => {
+    if(name == 'Hedy'){
+      setName('Ling');
+    }else{
+    setName('Hedy');
+  }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Event click</h2>
+      <p>{name}</p>
+      <button onClick={handleClick}>Click me</button>
+     
     </div>
   );
 }
